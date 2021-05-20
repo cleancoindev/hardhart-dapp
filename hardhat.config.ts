6d4381b6f1a302a4f663bf167e75b58f0e405ec5
Brett Henderson <bretth18@gmail.com>
2021-05-20 15:34:00 -0700

    set up typescript shit and react frontend

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,39 +1,78 @@
 // Only need to import waffle, it epends on hardhart-ethers.
 import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-ethers";
+import "hardhat-deploy-ethers";
+import "hardhat-deploy";
+import "@symfoni/hardhat-react";
+import "hardhat-typechain";
+import "@typechain/ethers-v5";
+import 'tsconfig-paths/register'; // adds support for typescript paths mappings
+
+import { HardhatUserConfig, task } from "hardhat/config";
+
 
 const fs = require('fs');
 const privateKey = fs.readFileSync(".secret").toString().trim();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
 
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-export default {
-  defaultNetwork: "matic",
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  react: {
+    providerPriority: ["web3modal", "hardhat"],
+  },
+
   networks: {
-    hardhat: {},
+    hardhat: {
+      inject: false, // optional. if true it will expose your mnemonic in front end. it would be available as an in page browser wallet/signer which can sign without confirmation.
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk", // test test test test test test test test test test test junk
+      },
+    },
     matic: {
       url: "https://rpc-mumbai.maticvigil.com",
       accounts: [privateKey]
-    }
+    },
   },
+
   solidity: {
-    version: "0.8.0",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
-    }
+    compilers: [ 
+      {
+        version: "0.8.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
   },
+
   paths: {
     sources: "./contracts",
     tests: "./test",
     cache: "./cache",
     artifacts: "./artifacts"
   },
+
   mocha: {
     timeout: 20000
-  }
+  },
+
 };
 
+
+export default config;
---
+++ scripts/deploy.ts
@@ -0,0 +1,45 @@
+//deployment script
+// usage
+// npx hardhat run scripts/deploy.ts --network <network-name>
+
+
+
+import { ethers } from "hardhat";
+
+
+
+async function main() {
+
+    // get out deployment address
+    const [deployer] = await ethers.getSigners();
+
+    console.log(
+        "Deploying contracts with the account:",
+        deployer.address
+    );
+
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+
+    // Deploy
+    const factory = await ethers.getContractFactory("PolyBread");
+    const contract = await factory.deploy();
+
+    // The address of the contract once mined
+    console.log("Contract address:", contract.address);
+
+    // Transaction that was sent to the network
+    console.log(contract.deployTransaction.hash);
+
+
+    // Contract not deployed yet, we must wait until block has been mined
+    await contract.deployed();
+
+}
+
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
