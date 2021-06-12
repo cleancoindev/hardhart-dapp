3ab5445a2954d06278cfe76ceb91e30430e817d2
brett <bretth18@gmail.com>
2021-06-11 19:35:21 -0700

    fixes

--- frontend/src/pages/nft/[nftId].tsx
+++ frontend/src/pages/nft/[nftId].tsx
@@ -6,7 +6,6 @@ import { SymfoniPbNFT } from '../../hardhat/SymfoniContext';
 
 import PbNFTDeployment from '../../hardhat/deployments/chainstack/PbNFT.json';
 
-import { PbNFT__factory } from '../../hardhat/typechain';
 
 import { PbNFTModel } from '../../components/NFTs/NFT';
 
@@ -33,79 +32,80 @@ async function getInitialProps(context: NextPageContext) {
 
     
 
-    // const provider = new ethers.providers.InfuraProvider("maticmum",   {projectId: INFURA_ID, projectSecret: INFURA_SECRET});
-    const provider =  ethers.getDefaultProvider(maticTestnet);
-    console.log('infura provider', provider);
+    // // const provider = new ethers.providers.InfuraProvider("maticmum",   {projectId: INFURA_ID, projectSecret: INFURA_SECRET});
+    // const provider =  ethers.getDefaultProvider(maticTestnet);
+    // console.log('infura provider', provider);
+    // PbNFT?.factory
 
-    const contractAddress = PbNFTDeployment.address;
-    const instance = PbNFT__factory.connect(contractAddress, provider);
+    // const contractAddress = PbNFTDeployment.address;
+    // const instance = PbNFT__factory.connect(contractAddress, provider);
 
-    const PbNFT: SymfoniPbNFT = {
-        instance: instance,
-        factory: undefined,
-    };
+    // const PbNFT: SymfoniPbNFT = {
+    //     instance: instance,
+    //     factory: undefined,
+    // };
 
-    let nftId = String(context.query.nftId);
+    // let nftId = String(context.query.nftId);
 
-    // should grab the instance from quering the tokenByIndex based on our passed Id.
-    let nft = await PbNFT.instance?.tokenByIndex(nftId);
+    // // should grab the instance from quering the tokenByIndex based on our passed Id.
+    // let nft = await PbNFT.instance?.tokenByIndex(nftId);
 
-    console.log('mft:',nft);
+    // console.log('mft:',nft);
 
-    if (nft) {
-        console.log(nft);
-        // maybe
-        nft = { ...nft, id: nftId } as any;
+    // if (nft) {
+    //     console.log(nft);
+    //     // maybe
+    //     nft = { ...nft, id: nftId } as any;
 
-        // Setup event filters to check if who owns this shit
-        const nftMintedEventFilter = PbNFT.instance?.filters?.NFTMinted(
-            null,
-            null,
-            BigNumber.from(nftId).toHexString(),
+    //     // Setup event filters to check if who owns this shit
+    //     const nftMintedEventFilter = PbNFT.instance?.filters?.NFTMinted(
+    //         null,
+    //         null,
+    //         BigNumber.from(nftId).toHexString(),
             
-        );
+    //     );
 
-        const transferEventFilter = PbNFT?.instance?.filters?.Transfer(null, null, BigNumber.from(nftId).toHexString());
+    //     const transferEventFilter = PbNFT?.instance?.filters?.Transfer(null, null, BigNumber.from(nftId).toHexString());
 
-        try {
-            const [nftMintedLog] = await provider?.getLogs({...nftMintedEventFilter, address: contractAddress, fromBlock: 'latest' });
-            const parsedNftMintedLog = PbNFT?.instance?.interface?.parseLog(nftMintedLog)?.args;
+    //     try {
+    //         const [nftMintedLog] = await provider?.getLogs({...nftMintedEventFilter, address: contractAddress, fromBlock: 'latest' });
+    //         const parsedNftMintedLog = PbNFT?.instance?.interface?.parseLog(nftMintedLog)?.args;
     
     
-            const transferLogs = await provider?.getLogs({...transferEventFilter, address: contractAddress,  fromBlock: 'latest'});
-            const parsedTransferLogs = PbNFT?.instance?.interface?.parseLog(transferLogs[transferLogs.length - 1])?.args;
+    //         const transferLogs = await provider?.getLogs({...transferEventFilter, address: contractAddress,  fromBlock: 'latest'});
+    //         const parsedTransferLogs = PbNFT?.instance?.interface?.parseLog(transferLogs[transferLogs.length - 1])?.args;
 
 
-            if (parsedNftMintedLog && parsedTransferLogs) {
-                const [createdBy] = parsedNftMintedLog;
-                const [, ownedBy] = parsedTransferLogs;
+    //         if (parsedNftMintedLog && parsedTransferLogs) {
+    //             const [createdBy] = parsedNftMintedLog;
+    //             const [, ownedBy] = parsedTransferLogs;
     
-                return {
-                    id: nftId,
-                    nft,
-                    createdBy: (await provider?.resolveName(createdBy)) || createdBy,
-                    ownedBy: (await provider?.resolveName(ownedBy)) || ownedBy,
-                };
-            } else if (parsedNftMintedLog && !parsedTransferLogs) {
-                console.log('no parse transfer found, elif condition hit');
+    //             return {
+    //                 id: nftId,
+    //                 nft,
+    //                 createdBy: (await provider?.resolveName(createdBy)) || createdBy,
+    //                 ownedBy: (await provider?.resolveName(ownedBy)) || ownedBy,
+    //             };
+    //         } else if (parsedNftMintedLog && !parsedTransferLogs) {
+    //             console.log('no parse transfer found, elif condition hit');
     
-                const [createdBy] = parsedNftMintedLog;
+    //             const [createdBy] = parsedNftMintedLog;
     
-                return {
-                    id: nftId,
-                    nft,
-                    createdBy: (await provider?.resolveName(createdBy)) || createdBy,
-                };
-            }
+    //             return {
+    //                 id: nftId,
+    //                 nft,
+    //                 createdBy: (await provider?.resolveName(createdBy)) || createdBy,
+    //             };
+    //         }
 
-        } catch (e) {
-            console.error(e);
-        }
-
-
+    //     } catch (e) {
+    //         console.error(e);
+    //     }
 
 
-    }
+
+
+    // }
 
 
     return {};
